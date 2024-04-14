aab21b3d2b432306328689973d4383ce021b59fb
Dane Martin <40708424@live.napier.ac.uk>
2024-04-14 11:42:34 -0500

    added comments to App

--- src/main/java/cw/group8/App.java
+++ src/main/java/cw/group8/App.java
@@ -1,7 +1,6 @@
 package cw.group8;
 
 import java.sql.*;
-import java.util.ArrayList;
 
 public class App
 {
@@ -16,109 +15,110 @@ public class App
         // Please comment out each issue once working properly
 
         //ID08 Get all countries population in the world
-        /**
-         ID08App ID08 = new ID08App();
-         ID08.printCities(ID08.getAllCountriesWorld(con));
+        /*
+             ID08App ID08 = new ID08App();
+             ID08.printCities(ID08.getAllCountriesWorld(con));
          */
 
 
         //ID09 Get all countries population in a continent
-        /**
+        /*
          ID09App ID09 = new ID09App();
          ID09.printCities(ID09.getAllCountriesContinent(con));
          */
         
         //ID10 Get all countries population in a region
-        /**
+        /*
          ID10App ID10 = new ID10App();
          ID10.printCities(ID10.getAllCountriesRegion(con));
          */
         
         //ID11 Get top countries population in the world
-        /**
+        /*
          ID11App ID11 = new ID11App();
          ID11.printCities(ID11.getTopCountriesWorld(con));
          */
         
         //ID12 Get top countries population in a continent
-        /**
+        /*
          ID12App ID12 = new ID12App();
          ID12.printCities(ID12.getTopCountriesContinent(con));
          */
         
         //ID13 Get top countries population in a region
-        /**
+        /*
          ID13App ID13 = new ID13App();
          ID13.printCities(ID13.getTopCountriesRegion(con));
          */
         
         //ID14 Get all city population in the world
-        /**
+        /*
          ID14App ID14 = new ID14App();
          ID14.printCities(ID14.getAllCitiesWorld(con));
          */
         
         //ID15 Get all city population in a continent
-        /**
+        /*
          ID15App ID15 = new ID15App();
          ID15.printCities(ID15.getAllCitiesContinent(con));
          */
         
         //ID16 Get all city population in a region
-        /**
+        /*
          ID16App ID16 = new ID16App();
          ID16.printCities(ID16.getAllCitiesRegion(con));
          */
         
         //ID17 Get all city population in a country
-        /**
+        /*
          ID17App ID17 = new ID17App();
          ID17.printCities(ID17.getAllCitiesCountry(con));
          */
         
         //ID18 Get all city population in a district
-        /**
+        /*
          ID18App ID18 = new ID18App();
          ID18.printCities(ID18.getAllCitiesDistrict(con));
          */
         
         //ID19 Get top city population in the world
-        /**
+        /*
          ID19App ID19 = new ID19App();
          ID19.printCities(ID19.getTopCitiesWorld(con));
          */
 
         //ID20 Get top city population in a continent
-        /**
+        /*
          ID20App ID20 = new ID20App();
          ID20.printCities(ID20.getTopCitiesContinent(con));
          */
 
         //ID21 Get top city population in a region
-        /**
+        /*
          ID21App ID21 = new ID21App();
          ID21.printCities(ID21.getTopCitiesRegion(con));
          */
 
         //ID22 Get top city population in a country
-        /**
+        /*
          ID22App ID22 = new ID22App();
          ID22.printCities(ID22.getTopCitiesCountry(con));
          */
 
         // ID23 Get top city population in a district
-         /**
+         /*
               ID23App ID23 = new ID23App();
               ID23.printCities(ID23.getTopCitiesDistrict(con));
           */
 
-        /**
+         // ID24 Get population of all capital cities in the world
+        /*
 
             ID24App ID24 = new ID24App();
             ID24.printCities(ID24.getAllCities(con));
          */
 
-
+        // ID25 Get population for all capital cities in a Continent
         ID25App ID25 = new ID25App();
         ID25.printCities(ID25.getAllCitiesContinent(con));
 
