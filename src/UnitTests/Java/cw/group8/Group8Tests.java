79b3933d33d1cffa4825b6160b1c23629f7cacd2
Dane Martin <40708424@live.napier.ac.uk>
2024-04-14 22:55:12 -0500

    updated unit test class.

--- src/UnitTests/Java/cw/group8/Group8Tests.java
+++ src/UnitTests/Java/cw/group8/Group8Tests.java
@@ -8,25 +8,36 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class Group8Tests
 {
-    static ID24App app;
+    static ID24App app24;
+    static ID27App app27;
 
     @BeforeAll
     static void init()
     {
-        app = new ID24App();
+        app24 = new ID24App();
+        app27 = new ID27App();
     }
 
+    //Unit testing for Issue ID 24
     @Test
     void ID24AppGetAllCitiesTestNull()
     {
-        app.getAllCities(null);
+        app24.getAllCities(null);
     }
 
     @Test
-    void ID24AppPrintCitiesTestNull()
-    {
-        app.printCities(null);
-    }
+    void ID24AppPrintCitiesTestNull() {app24.printCities(null); }
+
+   //Unit testing for Issue ID 27
+   @Test
+   void ID27AppGetAllCitiesTestNull()
+   {
+       app27.getAllCities(null);
+   }
+
+    @Test
+    void ID27AppPrintCitiesTestNull() {app27.printCities(null); }
+
 
 
 
